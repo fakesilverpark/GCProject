a39e7a190256344d1684e4ff99dd57091aa844d6
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    refactor(auth): LoginRequest에 Lombok @Getter 적용 및 불필요한 setter 제거

--- src/main/java/bssm/be/auth/dto/LoginRequest.java
+++ src/main/java/bssm/be/auth/dto/LoginRequest.java
@@ -1,27 +1,13 @@
 package bssm.be.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 
+@Getter
 public class LoginRequest {
     @NotBlank
     private String displayName;
 
     @NotBlank
     private String password;
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
