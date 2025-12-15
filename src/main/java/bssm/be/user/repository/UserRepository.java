e952a7aeeaef049ce07983d7120a1fda0fda579d
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(user): 사용자 조회 및 중복 검사를 위한 UserRepository 인터페이스 추가

--- src/main/java/bssm/be/user/repository/UserRepository.java
+++ src/main/java/bssm/be/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package bssm.be.user.repository;
 
 import bssm.be.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByDisplayName(String displayName);
+    boolean existsByEmail(String email);
+    boolean existsByDisplayName(String displayName);
 }
