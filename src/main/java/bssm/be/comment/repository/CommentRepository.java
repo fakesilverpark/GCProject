798871486e2879d38d840b6729282ebd00196df9
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(comment): 댓글 영속성 처리를 위한 CommentRepository 추가

---
+++ src/main/java/bssm/be/comment/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package bssm.be.comment.repository;
+
+import bssm.be.comment.domain.Comment;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
