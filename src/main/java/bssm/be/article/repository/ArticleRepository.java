f8612d54ad84d346252fce9a1660ab7f0adf1c9c
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(article): 게시글 영속성 처리를 위한 ArticleRepository 인터페이스 추가

---
+++ src/main/java/bssm/be/article/repository/ArticleRepository.java
@@ -0,0 +1,11 @@
+package bssm.be.article.repository;
+
+import bssm.be.article.domain.Article;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
