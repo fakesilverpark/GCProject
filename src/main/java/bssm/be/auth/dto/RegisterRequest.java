3b8b050eeac6b7b53faa5af6403dea6150f8c7dc
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    refactor(auth): RegisterRequest에 Lombok @Getter 적용하여 접근자 코드 간소화

--- src/main/java/bssm/be/auth/dto/RegisterRequest.java
+++ src/main/java/bssm/be/auth/dto/RegisterRequest.java
@@ -2,7 +2,9 @@ package bssm.be.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 
+@Getter
 public class RegisterRequest {
     @NotBlank
     private String displayName;
@@ -10,20 +12,4 @@ public class RegisterRequest {
     @Size(min = 6, message = "비밀번호는 6자 이상이어야 합니다.")
     @NotBlank
     private String password;
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
