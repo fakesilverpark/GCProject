5064e4c807e9b0652ba9081a959832b3961d8ed6
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(comment): 대댓글을 포함한 댓글 생성 서비스 로직 구현

--- src/main/java/bssm/be/comment/service/CommentService.java
+++ src/main/java/bssm/be/comment/service/CommentService.java
@@ -1,7 +1,13 @@
 package bssm.be.comment.service;
 
+import bssm.be.article.domain.Article;
 import bssm.be.article.repository.ArticleRepository;
+import bssm.be.comment.domain.Comment;
+import bssm.be.comment.dto.CommentCreateRequest;
+import bssm.be.comment.dto.CommentResponse;
 import bssm.be.comment.repository.CommentRepository;
+import bssm.be.common.exception.NotFoundException;
+import bssm.be.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,4 +19,22 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final ArticleRepository articleRepository;
+
+    public CommentResponse create(Long articleId, CommentCreateRequest request, User user) {
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new NotFoundException("게시글을 찾을 수 없습니다."));
+
+        String parentPath = null;
+        if (request.getParentId() != null) {
+            Comment parent = commentRepository.findByIdAndArticleId(request.getParentId(), articleId)
+                    .orElseThrow(() -> new NotFoundException("상위 댓글이 존재하지 않습니다."));
+            parentPath = parent.getPath();
+        }
+
+        Comment comment = new Comment(article, user, request.getContent());
+        commentRepository.save(comment);
+        comment.assignPath(parentPath);
+        commentRepository.save(comment);
+        return new CommentResponse(comment);
+    }
 }
