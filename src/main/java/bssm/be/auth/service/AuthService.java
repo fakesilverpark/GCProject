64aaf6c75ae89b1ec258025b35307d259d19952e
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(auth): 회원가입 및 로그인 비즈니스 로직을 처리하는 AuthService 구현 및 JWT 토큰 발급 로직 연동

--- src/main/java/bssm/be/auth/service/AuthService.java
+++ src/main/java/bssm/be/auth/service/AuthService.java
@@ -1,9 +1,32 @@
 package bssm.be.auth.service;
 
+import bssm.be.common.security.JwtTokenProvider;
+import bssm.be.user.domain.User;
+import bssm.be.user.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(UserService userService, JwtTokenProvider jwtTokenProvider) {
+        this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        User user = userService.register(request.getDisplayName(), request.getPassword());
+        String token = jwtTokenProvider.generateToken(user);
+        return new AuthResponse(token, user);
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        User user = userService.authenticate(request.getDisplayName(), request.getPassword());
+        String token = jwtTokenProvider.generateToken(user);
+        return new AuthResponse(token, user);
+    }
 }
