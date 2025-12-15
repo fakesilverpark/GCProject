03ce6c22ea4712c863aa5d5eb0cd47109aa25312
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(comment): 게시글 댓글 생성 API 엔드포인트 추가

--- src/main/java/bssm/be/comment/controller/CommentController.java
+++ src/main/java/bssm/be/comment/controller/CommentController.java
@@ -1,10 +1,14 @@
 package bssm.be.comment.controller;
 
 import bssm.be.comment.service.CommentService;
+import bssm.be.common.security.UserPrincipal;
+import bssm.be.user.domain.User;
 import bssm.be.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -13,4 +17,12 @@ public class CommentController {
 
     private final CommentService commentService;
     private final UserService userService;
+
+    @PostMapping("/articles/{articleId}/comments")
+    public ResponseEntity<CommentResponse> create(@AuthenticationPrincipal UserPrincipal principal,
+                                                  @PathVariable Long articleId,
+                                                  @Valid @RequestBody CommentCreateRequest request) {
+        User user = userService.findById(principal.getUserId());
+        return ResponseEntity.ok(commentService.create(articleId, request, user));
+    }
 }
