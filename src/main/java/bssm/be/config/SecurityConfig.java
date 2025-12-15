788b5d1d5b4c634dd5a106e52c1ab68397eb8ece
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(security): SecurityConfig에 JwtAuthenticationFilter 적용하여 JWT 인증 필터 체인에 등록

--- src/main/java/bssm/be/config/SecurityConfig.java
+++ src/main/java/bssm/be/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package bssm.be.config;
 
+import bssm.be.common.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
