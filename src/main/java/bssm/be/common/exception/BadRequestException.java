712692c9771dc5c1ca4f4dabc12de716a79b9a4e
eunbb <fakesilverpark@gmail.com>
2025-12-15 09:00:00 +0900

    feat(common): 잘못된 요청 상황을 처리하기 위한 BadRequestException 추가

---
+++ src/main/java/bssm/be/common/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package bssm.be.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends ApiException {
+    public BadRequestException(String message) {
+        super(HttpStatus.BAD_REQUEST, message);
+    }
+}
